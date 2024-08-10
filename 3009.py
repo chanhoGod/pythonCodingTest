83725d20e7bd261fa293806222a125afea8ee97b
DESKTOP-5N9E7F2\JeongChanho <cksgh3422@gmail.com>
2024-08-10 16:18:18 +0900

    pythonCodingTest_10101.py commit_2024-08-10

---
+++ 3009.py
@@ -0,0 +1,18 @@
+
+tmp_x = list()
+tmp_y = list()
+
+for i in range(3):
+    x, y = map(int, input().split(sep=" "))
+    
+    if x in tmp_x:
+        tmp_x.remove(x)
+    else:
+        tmp_x.append(x)
+        
+    if y in tmp_y:
+        tmp_y.remove(y)
+    else:
+        tmp_y.append(y)    
+    
+print(str(tmp_x[0]) + " " + str(tmp_y[0]))
