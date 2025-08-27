704fb4535352506c18563a7ffbc4d18d93b72e3f
jeongchanho <cksgh3422@gmail.com>
2025-08-28 02:24:04 +0900

    pythonCodingTest_1904

--- 9251.py
+++ 9251.py
@@ -1,13 +1,13 @@
-A = str(input())
-B = str(input())
-dp = [[0] * (len(B)+1) for _ in range(len(A)+1)]
+A = input().strip()
+B = input().strip()
 
-for i in range(1, len(A)+1):
-    for j in range(1, len(B)+1):
+DP = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+
+for i in range(1, len(A) + 1):
+    for j in range(1, len(B) + 1):
         if A[i-1] == B[j-1]:
-            dp[i][j] = dp[i-1][j-1] + 1
+            DP[i][j] = DP[i-1][j-1] + 1
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            DP[i][j] = max(DP[i][j-1], DP[i-1][j])
         
-
-print(dp[len(A)][len(B)])
+print(DP[len(A)][len(B)])
